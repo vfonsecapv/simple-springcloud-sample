d048c79e21518393308a11eadd0727123ee5d82f
victorfonseca <victortales@gmail.com>
2015-07-14 11:52:17 -0300

    .

--- hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
+++ hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.feign.EnableFeignClients
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
 @EnableZuulProxy
+@EnableFeignClients
 class HelloYouApplication {
     static void main(String[] args) {
         SpringApplication.run(HelloYouApplication.class, args)
