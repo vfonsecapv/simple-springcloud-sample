4446f56f6b1c7f6915fb4b835031b145129daa90
victorfonseca <victortales@gmail.com>
2015-07-23 09:55:24 -0300

    .

--- eureka-server/src/main/groovy/eurekaserver/EurekaServer.groovy
+++ eureka-server/src/main/groovy/eurekaserver/EurekaServer.groovy
@@ -1,10 +1,12 @@
 package eurekaserver
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @EnableEurekaServer
+@EnableAutoConfiguration
 @SpringBootApplication
 class EurekaServer {
 
