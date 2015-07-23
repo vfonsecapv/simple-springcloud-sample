974ca758a6ecd0576461f196f994834c5ab57dae
victorfonseca <victortales@gmail.com>
2015-07-23 08:41:13 -0300

    .

--- hello-service/src/main/groovy/helloservice/rest/HelloYouService.groovy
+++ hello-service/src/main/groovy/helloservice/rest/HelloYouService.groovy
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.client.ServiceInstance
 import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RefreshScope
 @RestController
+@Component
 class HelloYouService {
 	@Autowired
 	DiscoveryClient client
