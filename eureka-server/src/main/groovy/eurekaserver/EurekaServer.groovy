ce47b4086a5eb07237cb3f9a558b452aad1ef1a5
victorfonseca <victortales@gmail.com>
2015-07-23 16:13:22 -0300

    .

--- eureka-server/src/main/groovy/eurekaserver/EurekaServer.groovy
+++ eureka-server/src/main/groovy/eurekaserver/EurekaServer.groovy
@@ -1,12 +1,10 @@
 package eurekaserver
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @EnableEurekaServer
-@EnableAutoConfiguration
 @SpringBootApplication
 class EurekaServer {
 
--- hello-service/src/main/groovy/helloservice/rest/HelloYouService.groovy
+++ hello-service/src/main/groovy/helloservice/rest/HelloYouService.groovy
@@ -1,9 +1,6 @@
 package helloservice.rest
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cloud.client.ServiceInstance
-import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -12,16 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 @RefreshScope
 @RestController
 class HelloYouService {
-	@Autowired
-	DiscoveryClient client
-
     @Value('${message}')
     def message
 
 	@RequestMapping(name = "/", method = RequestMethod.GET,  produces = "application/json;charset=utf-8")
     String hi() {
-    	ServiceInstance localInstance = client.getLocalServiceInstance()
-
-    	"Hello for you! INS: " + message + " : " +localInstance.getServiceId()+":"+localInstance.getHost()+":"+localInstance.getPort()
+    	"Olá, " + message
     }
 }
