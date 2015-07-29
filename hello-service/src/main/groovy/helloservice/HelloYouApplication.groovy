c8027d7ce73bee4bdbd32fe76b5d8d7869ebca86
Victor Fonseca <vfonseca@pivotal.io>
2015-07-29 10:10:12 -0300

    Change cliente

--- hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
+++ hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
@@ -3,11 +3,9 @@ package helloservice
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.netflix.ribbon.RibbonClient
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RibbonClient(name = "hello-service", configuration = HelloYourProducer.class)
 class HelloYouApplication {
     static void main(String[] args) {
         SpringApplication.run HelloYouApplication.class, args
