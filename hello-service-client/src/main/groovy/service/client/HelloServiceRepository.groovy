0e056099bcd56ebafde157be625004472ce52202
Victor Fonseca <vfonseca@pivotal.io>
2015-10-20 14:00:35 -0200

    Update

--- hello-service-client/src/main/groovy/service/client/HelloServiceRepository.groovy
+++ hello-service-client/src/main/groovy/service/client/HelloServiceRepository.groovy
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
-@FeignClient("hello-service")
+@FeignClient("HELLO-SERVICE")
 interface HelloServiceRepository {
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     String sayHello()
