b1f88fd2403f153f1361b61c5caba79526c2bdd0
Victor Fonseca <vfonseca@pivotal.io>
2015-10-20 11:57:17 -0200

    Running on PWS

--- hystrix-dashboard/src/main/groovy/circuitbreaker/CircuitBreakerHystrix.groovy
+++ hystrix-dashboard/src/main/groovy/circuitbreaker/CircuitBreakerHystrix.groovy
@@ -3,12 +3,10 @@ package circuitBreaker
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
 @EnableHystrixDashboard
 @SpringBootApplication
-@Controller
 class CircuitBreakerHystrix {
 	@RequestMapping("/")
 	public String home() {
