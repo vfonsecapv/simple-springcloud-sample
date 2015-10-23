1b8524ff7e1f11e8cd09e38babb5ecb7235a8d46
Victor Fonseca <vfonseca@pivotal.io>
2015-10-23 15:04:51 -0200

    Update PangeaNet

--- eureka-server/src/main/groovy/eurekaserver/EurekaServer.groovy
+++ eureka-server/src/main/groovy/eurekaserver/EurekaServer.groovy
@@ -2,9 +2,11 @@ package eurekaserver
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 class EurekaServer {
 
--- hello-service-client/src/main/groovy/service/HelloService.groovy
+++ hello-service-client/src/main/groovy/service/HelloService.groovy
@@ -6,7 +6,9 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.feign.EnableFeignClients
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableEurekaClient
 @EnableCircuitBreaker
