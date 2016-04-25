88ad30872158a971371bccea5871ed201e8cdcb3
Victor Fonseca <vfonseca@pivotal.io>
2016-04-25 09:12:05 -0300

    Update manifest.yml hystrix

--- hello-service-client/src/main/groovy/service/controller/HelloController.groovy
+++ hello-service-client/src/main/groovy/service/controller/HelloController.groovy
@@ -2,13 +2,11 @@ package service.controller
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import service.client.HelloServiceRepository
 
-@RefreshScope
 @RestController
 @RequestMapping("/hello")
 class HelloController {
