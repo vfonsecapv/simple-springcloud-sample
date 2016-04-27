ae36ccef01e14182c63762322d19b5911806feeb
Victor Fonseca <vfonseca@pivotal.io>
2016-04-27 18:10:23 -0300

    New build

--- hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
+++ hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
@@ -3,7 +3,11 @@ package helloservice
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.feign.EnableFeignClients
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+@EnableZuulProxy
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 class HelloYouApplication {
