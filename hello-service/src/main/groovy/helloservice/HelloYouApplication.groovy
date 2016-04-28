7b950c3c0832eaa46e52a90d777295f9836a9534
Victor Fonseca <vfonseca@pivotal.io>
2016-04-27 22:46:47 -0300

    New build

--- hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
+++ hello-service/src/main/groovy/helloservice/HelloYouApplication.groovy
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.feign.EnableFeignClients
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
-@EnableZuulProxy
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
